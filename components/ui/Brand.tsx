5627066f15c404a705339db0186edb8752a2ffd5
KevTLW <kevinmcruzc@gmail.com>
2022-02-17 16:46:22 -0500

    [WEB] add brand component

---
+++ components/ui/Brand.tsx
@@ -0,0 +1,14 @@
+interface BrandProps {
+  accent: string;
+  primary: string;
+}
+
+export const Brand = ({ accent, primary }: BrandProps) => {
+  return (
+    <>
+      <span className={`${primary} [font-variant:small-caps]`}>j</span>
+      <span className={accent}>o</span>
+      <span className={primary}>ckey</span>
+    </>
+  );
+};
