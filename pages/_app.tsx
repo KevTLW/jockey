8971935e24fa968bd8b027b256756a3f4b74315d
KevTLW <kevinmcruzc@gmail.com>
2022-02-27 12:26:35 -0500

    [WEB] add transition to entire app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import "tailwindcss/tailwind.css";
 
 export const _App = ({ Component, pageProps }: AppProps) => {
   return (
-    <div className="bg-gray-100 font-mono antialiased dark:bg-gray-900">
+    <div className="bg-gray-100 font-mono antialiased transition dark:bg-gray-900">
       <div className="container mx-auto">
         <Component {...pageProps} />
       </div>
