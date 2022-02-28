eb0bfe9403d822b71572f1bf80554f82aade6011
KevTLW <kevinmcruzc@gmail.com>
2022-02-27 19:29:13 -0500

    [WEB] migrate dark mode handling to next-themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 
 export const _App = ({ Component, pageProps }: AppProps) => {
   return (
-    <div className="bg-gray-100 font-mono antialiased transition dark:bg-gray-900">
-      <div className="container mx-auto">
-        <Component {...pageProps} />
+    <ThemeProvider attribute="class" defaultTheme="system">
+      <div className="bg-slate-50 font-mono antialiased transition dark:bg-slate-900">
+        <div className="container mx-auto">
+          <Component {...pageProps} />
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 };
 
