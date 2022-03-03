e9bb1e6b7113220efa543e6694ece155a7483174
KevTLW <kevinmcruzc@gmail.com>
2022-03-03 11:17:18 -0500

    [WEB] add option for custom classes to ui elements

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -6,10 +6,12 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const Button = ({
   children,
+  className,
   theme = "default",
   ...props
 }: ButtonProps) => {
   const classes = [
+    className,
     "rounded border-2 bg-transparent px-4 py-2 text-center font-bold shadow-xl transition duration-300 focus:outline-none focus:ring",
     theme === "primary" &&
       "border-sky-700 text-sky-700 shadow-sky-500/25 hover:bg-sky-700 hover:text-slate-50 focus:ring-sky-700/75 active:bg-sky-800 active:text-slate-50 dark:border-sky-300 dark:text-sky-300 dark:shadow-sky-300/25 dark:hover:bg-sky-300 dark:hover:text-slate-900 dark:focus:ring-sky-300/75 dark:active:bg-sky-400 dark:active:text-slate-900",
--- components/ui/Link.client.tsx
+++ components/ui/Link.client.tsx
@@ -3,11 +3,13 @@ import { ReactNode } from "react";
 
 interface LinkProps extends NextLinkProps {
   theme?: "default" | "primary";
+  className?: string;
   children?: ReactNode;
 }
 
-const Link = ({ children, href, theme }: LinkProps) => {
+const Link = ({ children, className, href, theme }: LinkProps) => {
   const classes = [
+    className,
     "inline-block rounded border-2 bg-transparent px-4 py-2 text-center font-bold shadow-xl transition duration-300 focus:outline-none focus:ring",
     theme === "primary" &&
       "border-sky-700 text-sky-700 shadow-sky-500/25 hover:bg-sky-700 hover:text-slate-50 focus:ring-sky-700/75 active:bg-sky-800 active:text-slate-50 dark:border-sky-300 dark:text-sky-300 dark:shadow-sky-300/25 dark:hover:bg-sky-300 dark:hover:text-slate-900 dark:focus:ring-sky-300/75 dark:active:bg-sky-400 dark:active:text-slate-900",
