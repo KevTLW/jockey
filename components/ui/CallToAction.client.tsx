8150d2ac940e059fc7d2ec4b56853a5df20a81bc
KevTLW <kevinmcruzc@gmail.com>
2022-03-24 19:21:07 -0400

    [WEB] update call to action to use Link when necessary

--- components/ui/CallToAction.client.tsx
+++ components/ui/CallToAction.client.tsx
@@ -1,30 +1,27 @@
-import { useAtomValue } from "jotai";
-import { useRouter } from "next/router";
-import { useEffect, useReducer, useState } from "react";
+import { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth_errorAtom } from "../../state/atoms";
 import { auth } from "../../utils/firebase";
 import Button from "./Button.client";
+import Link from "./Link.client";
 import Modal from "./Modal.client";
 
 const CallToAction = () => {
-  const router = useRouter();
   const [user] = useAuthState(auth);
   const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
 
   const handleCallToAction = () => {
-    if (user) {
-      router.push("/party");
-    } else {
-      setLoginModalIsOpen(true);
-    }
+    setLoginModalIsOpen(true);
   };
 
   return (
     <>
-      <Button theme="primary" onClick={handleCallToAction}>
-        use jockey
-      </Button>
+      {user ? (
+        <Link href="/party">use jockey</Link>
+      ) : (
+        <Button theme="primary" onClick={handleCallToAction}>
+          use jockey
+        </Button>
+      )}
       <Modal isOpen={loginModalIsOpen} setIsOpen={setLoginModalIsOpen} />
     </>
   );
