a398996aaff18475525b4f6cf326471a48538778
KevTLW <kevinmcruzc@gmail.com>
2022-03-29 15:28:37 -0400

    [WEB] add query client provider for react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
 import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "tailwindcss/tailwind.css";
 
 export const _App = ({ Component, pageProps }: AppProps) => {
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider attribute="class" defaultTheme="system">
-      <div className="bg-slate-50 font-mono antialiased transition dark:bg-slate-900">
-        <div className="container mx-auto">
-          <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system">
+        <div className="bg-slate-50 font-mono antialiased transition dark:bg-slate-900">
+          <div className="container mx-auto">
+            <Component {...pageProps} />
+          </div>
         </div>
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
