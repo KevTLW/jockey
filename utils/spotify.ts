9197258ed319893e5faf2b8ec4664b776b883bbf
KevTLW <kevinmcruzc@gmail.com>
2022-03-29 15:04:13 -0400

    [WEB] add function to perform a spotify search

--- utils/spotify.ts
+++ utils/spotify.ts
@@ -1,3 +1,31 @@
+interface Track {
+  items: TrackItem[];
+}
+
+interface TrackItem {
+  album: Album;
+  artists: Artist[];
+  explicit: boolean;
+  id: string;
+  name: string;
+}
+
+interface Artist {
+  name: string;
+}
+
+interface Album {
+  images: Image[];
+}
+
+interface Image {
+  url: string;
+}
+
+interface SpotifySearchResult {
+  tracks: Track;
+}
+
 const getSpotifyAccessToken = async () => {
   const spotifyClientPair = `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`;
   const spotifyApiKey = Buffer.from(spotifyClientPair).toString("base64");
@@ -19,6 +47,26 @@ const getSpotifyAccessToken = async () => {
     ).then((res) => res.json());
 
     return accessToken;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const searchSpotify = async (query: string) => {
+  try {
+    const encodedQuery = encodeURIComponent(query);
+    const accessToken = await getSpotifyAccessToken();
+
+    const data: SpotifySearchResult = await fetch(
+      `https://api.spotify.com/v1/search?type=track&q=${encodedQuery}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    ).then((res) => res.json());
+
+    return data;
   } catch (error) {
     console.error(error);
   }
