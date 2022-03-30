4744c27d1708f8b3a9bc80c9143ca79b4298706d
KevTLW <kevinmcruzc@gmail.com>
2022-03-30 00:23:50 -0400

    [WEB] add hook to get party information

---
+++ queries/useParty.ts
@@ -0,0 +1,29 @@
+import {
+  doc,
+  DocumentReference,
+  Firestore,
+  FirestoreError,
+} from "firebase/firestore";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+export interface Party {
+  host: string;
+  createdAt: Date;
+}
+
+export interface UseParty {
+  party: Party | undefined;
+  loading: boolean;
+  error: FirestoreError | undefined;
+}
+
+export const useParty = (db: Firestore, id: string): UseParty => {
+  const partyRef = doc(db, "parties", id) as DocumentReference<Party>;
+  const [party, loading, error] = useDocumentData(partyRef);
+
+  return {
+    party,
+    loading,
+    error,
+  };
+};
