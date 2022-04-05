92c87f0f5184f4b18cfcf162427e820704b892d6
KevTLW <kevinmcruzc@gmail.com>
2022-04-04 23:14:38 -0400

    [WEB] add component to view all requests

---
+++ components/party/Requests.client.tsx
@@ -0,0 +1,52 @@
+import { User } from "firebase/auth";
+import { NextRouter } from "next/router";
+import { Party } from "../../queries/useParty";
+import { Request } from "../../queries/useRequests";
+import LoadingSong from "./LoadingSong.client";
+import Song from "./Song.client";
+
+interface RequestsProps {
+  party: Party | undefined;
+  requests: Request[] | undefined;
+  loading: boolean;
+  router: NextRouter;
+  user: User | null | undefined;
+}
+
+export const Requests = ({
+  party,
+  requests,
+  loading,
+  router,
+  user,
+}: RequestsProps) => {
+  if (!loading) {
+    return <LoadingSong amount={3} />;
+  }
+
+  if (requests?.length === 0) {
+    return (
+      <h1 className="font-semibold text-slate-500 dark:text-slate-400">
+        make requests on the search tab
+      </h1>
+    );
+  }
+
+  return (
+    <ul className="mx-auto w-full max-w-xl space-y-4">
+      {requests?.map((request) => (
+        <Song
+          party={party}
+          request={request}
+          type="request"
+          user={user}
+          key={request.id}
+          requests={requests}
+          router={router}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Requests;
