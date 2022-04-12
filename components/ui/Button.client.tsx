6c78f1dee9bec31ef2a5b0439c936990e7131c7f
KevTLW <kevinmcruzc@gmail.com>
2022-04-12 16:44:49 -0400

    [WEB] add special and special-inverse button variants

--- components/ui/Button.client.tsx
+++ components/ui/Button.client.tsx
@@ -6,9 +6,11 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     | "primary"
     | "positive"
     | "danger"
+    | "special"
     | "primary-inverse"
     | "positive-inverse"
-    | "danger-inverse";
+    | "danger-inverse"
+    | "special-inverse";
 }
 
 export const Button = ({
@@ -26,12 +28,16 @@ export const Button = ({
       "border-emerald-700 text-emerald-700 shadow-emerald-500/25 hover:bg-emerald-700 hover:text-slate-50 focus:ring-emerald-700/75 active:bg-emerald-800 active:text-slate-50 dark:border-emerald-300 dark:text-emerald-300 dark:shadow-emerald-300/25 dark:hover:bg-emerald-300 dark:hover:text-slate-900 dark:focus:ring-emerald-300/75 dark:active:bg-emerald-400 dark:active:text-slate-900",
     theme === "danger" &&
       "border-rose-700 text-rose-700 shadow-rose-500/25 hover:bg-rose-700 hover:text-slate-50 focus:ring-rose-700/75 active:bg-rose-800 active:text-slate-50 dark:border-rose-300 dark:text-rose-300 dark:shadow-rose-300/25 dark:hover:bg-rose-300 dark:hover:text-slate-900 dark:focus:ring-rose-300/75 dark:active:bg-rose-400 dark:active:text-slate-900",
+    theme === "special" &&
+      "border-violet-700 text-violet-700 shadow-violet-500/25 hover:bg-violet-700 hover:text-slate-50 focus:ring-violet-700/75 active:bg-violet-800 active:text-slate-50 dark:border-violet-300 dark:text-violet-300 dark:shadow-violet-300/25 dark:hover:bg-violet-300 dark:hover:text-slate-900 dark:focus:ring-violet-300/75 dark:active:bg-violet-400 dark:active:text-slate-900",
     theme === "primary-inverse" &&
       "border-sky-300 text-sky-300 shadow-sky-300/25 hover:bg-sky-300 hover:text-slate-900 focus:ring-sky-300/75 active:bg-sky-400 active:text-slate-900 dark:border-sky-700 dark:text-sky-700 dark:shadow-sky-500/25 dark:hover:bg-sky-700 dark:hover:text-slate-50 dark:focus:ring-sky-700/75 dark:active:bg-sky-800 dark:active:text-slate-50",
     theme === "positive-inverse" &&
       "border-emerald-300 text-emerald-300 shadow-emerald-300/25 hover:bg-emerald-300 hover:text-slate-900 focus:ring-emerald-300/75 active:bg-emerald-400 active:text-slate-900 dark:border-emerald-700 dark:text-emerald-700 dark:shadow-emerald-500/25 dark:hover:bg-emerald-700 dark:hover:text-slate-50 dark:focus:ring-emerald-700/75 dark:active:bg-emerald-800 dark:active:text-slate-50",
     theme === "danger-inverse" &&
       "border-rose-300 text-rose-300 shadow-rose-300/25 hover:bg-rose-300 hover:text-slate-900 focus:ring-rose-300/75 active:bg-rose-400 active:text-slate-900 dark:border-rose-700 dark:text-rose-700 dark:shadow-rose-500/25 dark:hover:bg-rose-700 dark:hover:text-slate-50 dark:focus:ring-rose-700/75 dark:active:bg-rose-800 dark:active:text-slate-50",
+    theme === "special-inverse" &&
+      "border-violet-300 text-violet-300 shadow-violet-300/25 hover:bg-violet-300 hover:text-slate-900 focus:ring-violet-300/75 active:bg-violet-400 active:text-slate-900 dark:border-violet-700 dark:text-violet-700 dark:shadow-violet-500/25 dark:hover:bg-violet-700 dark:hover:text-slate-50 dark:focus:ring-violet-700/75 dark:active:bg-violet-800 dark:active:text-slate-50",
   ];
 
   return (
